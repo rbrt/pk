a02d64746b3ab8e04996252284784a3fee15414c
Rob Butler <rob@orpheusinteractive.com>
2014-09-01 19:25:14 -0300

    Making attack templates so that reusing attacks in the AttackTree isn't a pain

---
+++ Assets/Scripts/Editor/PlayerAttackTemplateInspector.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(PlayerAttackTemplate))]
+public class PlayerAttackTemplateInspector : Editor {
+
+    public override void OnInspectorGUI(){
+
+        GUILayout.Label(target.name + ":", EditorStyles.boldLabel);
+
+        DrawDefaultInspector();
+
+    }
+
+    [MenuItem("PlayerAttack/Create PlayerAttack Template")]
+    public static void CreatePlayerAttackTemplate(){
+        string path = "Assets/ScriptableObjects/PlayerAttacks/New PlayerAttackTemplate.asset";
+        var item = ScriptableObject.CreateInstance("PlayerAttackTemplate");
+
+        AssetDatabase.CreateAsset(item, path);
+        AssetDatabase.SaveAssets();
+
+        Selection.activeObject = item;
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerAttackTemplate.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerAttackTemplate : ScriptableObject {
+
+    [SerializeField] protected float damage,
+                                     baseAttackTime,    // How long before other actions are allowed
+                                     attackRange,
+                                     attackDuration;
+
+    [SerializeField] protected Sprite[] attackSprites;
+
+    public float Damage{
+        get { return damage; }
+        set { damage = value; }
+    }
+
+    public float BaseAttackTime{
+        get { return baseAttackTime; }
+        set { baseAttackTime = value; }
+    }
+
+    public float AttackRange{
+        get { return attackRange; }
+        set { attackRange = value; }
+    }
+
+    public float AttackDuration{
+        get { return attackDuration; }
+        set { attackDuration = value; }
+    }
+
+}
