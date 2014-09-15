1874fa1deb955dc4581c4031fc5994baa7aac144
Rob Butler <robbutler902@gmail.com>
2014-09-14 21:05:34 -0300

    Added knockback effect and updated attack tree prefab

---
+++ Assets/Scripts/AttackEffects/KnockbackAttackEffect.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KnockbackAttackEffect : AttackEffect {
+
+    protected override IEnumerator Effect(GameObject target){
+        var basePos = target.transform.position;
+        var currentPos = basePos;
+        var trans = target.transform;
+
+        var animator = target.GetComponent<AnimateEnemy>();
+        animator.Damage();
+
+        float count = 0,
+              increment = .02f;
+
+
+        bool goRight = target.transform.rotation.y != 0;
+
+        while (count < 1){
+            if (goRight){
+                currentPos.x += increment;
+            }
+            else{
+                currentPos.x -= increment;
+            }
+
+            count += .3f;
+            trans.position = currentPos;
+
+            yield return null;
+        }
+    }
+
+}
